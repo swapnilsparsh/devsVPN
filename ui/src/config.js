04fc4d2a463a7b8e937658192d9f62a53a9b4d7d
Tarun Agarwal <tarun.agarwal@gocomet.com>
2024-05-18 22:17:24 +0530

    updated privateLine link in allowed URLs

--- ui/src/config.js
+++ ui/src/config.js
@@ -39,7 +39,7 @@ export default {
   UpdateWindowWidth: 600,
 
   // shellOpenExternal(...) allows only URLs started with this prefix
-  URLsAllowedPrefixes: ["https://www.ivpn.net/", "https://ivpn.net/", "x-apple.systempreferences:"],
+  URLsAllowedPrefixes: ["https://privateline.io/", "x-apple.systempreferences:"],
   URLApps: "https://www.ivpn.net/apps/",
 
   IsDebug,
