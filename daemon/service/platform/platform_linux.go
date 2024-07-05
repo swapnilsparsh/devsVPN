934bb4b4a5ab5a59b81a4e5bbcf22faa10245b80
Vladimir Zbarsky <zbarsky@cornell.edu>
2024-07-05 00:57:07 -0500

    Renamed programs, folders from privateline to privateline-connect prefix

--- daemon/service/platform/platform_linux.go
+++ daemon/service/platform/platform_linux.go
@@ -1,23 +1,23 @@
 //
-//  Daemon for privateLINE Client Desktop
+//  Daemon for privateLINE Connect Desktop
 //  https://github.com/swapnilsparsh/devsVPN
 //
 //  Created by Stelnykovych Alexandr.
 //  Copyright (c) 2023 IVPN Limited.
 //
-//  This file is part of the Daemon for privateLINE Client Desktop.
+//  This file is part of the Daemon for privateLINE Connect Desktop.
 //
-//  The Daemon for privateLINE Client Desktop is free software: you can redistribute it and/or
+//  The Daemon for privateLINE Connect Desktop is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License as published by the Free
 //  Software Foundation, either version 3 of the License, or (at your option) any later version.
 //
-//  The Daemon for privateLINE Client Desktop is distributed in the hope that it will be useful,
+//  The Daemon for privateLINE Connect Desktop is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 //  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 //  details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with the Daemon for privateLINE Client Desktop. If not, see <https://www.gnu.org/licenses/>.
+//  along with the Daemon for privateLINE Connect Desktop. If not, see <https://www.gnu.org/licenses/>.
 //
 
 package platform
