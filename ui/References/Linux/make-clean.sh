2652e499e7d6a6eefa5a5201d867eea2946729ac
Vladimir Zbarsky <zbarsky@cornell.edu>
2024-07-15 02:53:05 -0500

    'make clean' script for Linux

---
+++ ui/References/Linux/make-clean.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+OURDIR=$(dirname $0)
+cd "$OURDIR/../../../"			|| { RET=$?; >&2 echo "ERROR $RET cd"; exit $RET; }
+
+rm -rf cli/References/Linux/{_out_bin,_tmp}
+
+# don't delete compiled binaries under daemon/References/Linux/_deps/*_inst - they don't take much space
+#rm -rf daemon/References/Linux/_deps/*_inst
+rm -rf daemon/References/Linux/_deps/*_build
+rm -rf daemon/References/Linux/_deps/kem-helper/liboqs
+
+rm -rf daemon/References/Linux/scripts/_out_bin
+
+rm -rf ui/{dist,node_modules,out}
+rm -rf ui/References/Linux/{_out_bin,_tmp}
