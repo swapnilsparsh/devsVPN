8dc8434b4924c32107f8254be7eeffbb8365dee6
Sandeep Shukla <sandeepini.2012@gmail.com>
2024-12-09 07:37:47 +0530

    qr code in account for showing account id

--- ui/src/components/settings/settings-account.vue
+++ ui/src/components/settings/settings-account.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="flexColumn" style="justify-content: space-between; width: 100%">
     <div class="flexColumn" style="gap: 2rem">
-      <div>
-        <div class="settingsTitle">ACCOUNT DETAILS</div>
-        <div class="flexRowSpace" style="align-items: flex-start">
-          <div v-if="isProcessing" class="flexColumn" style="gap: 10px">
-            <ShimmerEffect :width="'100px'" :height="'100px'" :border-radius="'100%'" />
-            <ShimmerEffect v-for="(item, index) in accountShimmerItems" :key="index" :width="'350px'"
-              :height="'20px'" />
-          </div>
-          <div v-else-if="$store.state.account.userDetails.id" class="flexColumn">
-            <img v-if="!profileImage" src="@/assets/avtar.svg" style="height: 100px; width: 100px" />
-            <img v-else :src="profileImage" style="
+      <div class="flexRow">
+        <div>
+          <div class="settingsTitle">ACCOUNT DETAILS</div>
+          <div class="flexRowSpace" style="align-items: flex-start">
+            <div v-if="isProcessing" class="flexColumn" style="gap: 10px">
+              <ShimmerEffect :width="'100px'" :height="'100px'" :border-radius="'100%'" />
+              <ShimmerEffect v-for="(item, index) in accountShimmerItems" :key="index" :width="'350px'"
+                :height="'20px'" />
+            </div>
+            <div v-else-if="$store.state.account.userDetails.id" class="flexColumn">
+              <img v-if="!profileImage" src="@/assets/avtar.svg" style="height: 100px; width: 100px" />
+              <img v-else :src="profileImage" style="
                 height: 100px;
                 width: 100px;
                 border-radius: 100%;
@@ -19,53 +20,63 @@
                 margin-bottom: 10px;
               " />
 
-            <div>
-              <div class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Name:</div>
-                <div class="detailedParamValue">
-                  {{ $store.state.account.userDetails.name }}
+              <div>
+                <div class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Name:</div>
+                  <div class="detailedParamValue">
+                    {{ $store.state.account.userDetails.name }}
+                  </div>
                 </div>
-              </div>
-              <div v-if="this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Account ID:</div>
-                <div class="detailedParamValue">
-                  {{ this.$store.state.account.session.AccountID }}
+                <div v-if="this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Account ID:</div>
+                  <div class="detailedParamValue">
+                    {{ this.$store.state.account.session.AccountID }}
+                  </div>
                 </div>
-              </div>
-              <div v-if="!this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Email:</div>
-                <div class="detailedParamValue">
-                  {{ $store.state.account.userDetails.email }}
+                <div v-if="!this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Email:</div>
+                  <div class="detailedParamValue">
+                    {{ $store.state.account.userDetails.email }}
+                  </div>
                 </div>
-              </div>
-              <div v-if="!this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Phone:</div>
-                <div class="detailedParamValue">
-                  {{ $store.state.account.userDetails.phone }}
+                <div v-if="!this.IsAccIdLogin" class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Phone:</div>
+                  <div class="detailedParamValue">
+                    {{ $store.state.account.userDetails.phone }}
+                  </div>
                 </div>
-              </div>
 
-              <div class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Account Created on:</div>
-                <div class="detailedParamValue">
-                  {{ formattedCreatedAt }}
+                <div class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Account Created on:</div>
+                  <div class="detailedParamValue">
+                    {{ formattedCreatedAt }}
+                  </div>
                 </div>
-              </div>
 
-              <div class="flexRow paramBlockDetailedConfig">
-                <div class="defColor paramName">Account verification:</div>
-                <div class="detailedParamValue">
-                  {{
-                    $store.state.account.userDetails.isVerified
-                      ? "Done"
-                      : "Needed"
-                  }}
+                <div class="flexRow paramBlockDetailedConfig">
+                  <div class="defColor paramName">Account verification:</div>
+                  <div class="detailedParamValue">
+                    {{
+                      $store.state.account.userDetails.isVerified
+                        ? "Done"
+                        : "Needed"
+                    }}
+                  </div>
                 </div>
               </div>
             </div>
+            <div v-else>User profile data wasn't retrieved yet, please check later.</div>
+            <!-- <div v-else>Api Error: Data couldn't be fetched at this moment.</div> -->
           </div>
-          <div v-else>User profile data wasn't retrieved yet, please check later.</div>
-          <!-- <div v-else>Api Error: Data couldn't be fetched at this moment.</div> -->
+        </div>
+        <div class="overlay-container" style="margin: auto;" @click="toggleAccountIDBlur" title="Click to show or hide QR code">
+          <div ref="qrcode" :class="{ blurred: isAccountIDBlurred }"></div>
+          <div v-if="isAccountIDBlurred" class="overlay">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" fill="black" viewBox="0 0 512 512">
+              <path
+                d="M 36.01223241590214 59.49847094801223 Q 21.137614678899084 50.103975535168196 9.394495412844037 62.62996941896024 Q 0 77.5045871559633 12.525993883792049 89.24770642201835 L 475.98776758409787 452.50152905198775 L 475.98776758409787 452.50152905198775 Q 490.86238532110093 461.8960244648318 502.60550458715596 449.37003058103977 Q 512 434.4954128440367 499.47400611620793 422.7522935779817 L 417.27217125382265 358.5565749235474 L 417.27217125382265 358.5565749235474 Q 439.97553516819573 334.28746177370033 455.6330275229358 309.2354740061162 Q 471.2905198776758 284.9663608562691 479.9021406727829 265.39449541284404 Q 483.0336391437309 256 479.9021406727829 246.60550458715596 Q 470.50764525993884 224.68501529051989 452.50152905198775 197.28440366972478 Q 434.4954128440367 169.88379204892968 407.0948012232416 144.04892966360856 Q 378.91131498470946 117.43119266055047 341.3333333333333 99.42507645259938 Q 303.75535168195717 81.41896024464832 256 80.63608562691131 Q 216.07339449541286 81.41896024464832 182.40978593272172 93.94495412844037 Q 149.5290519877676 107.25382262996942 123.69418960244649 128.3914373088685 L 36.01223241590214 59.49847094801223 L 36.01223241590214 59.49847094801223 Z M 180.0611620795107 173.01529051987768 Q 211.37614678899084 144.04892966360856 256 143.26605504587155 Q 303.75535168195717 144.83180428134557 335.8532110091743 176.14678899082568 Q 367.16819571865443 208.2446483180428 368.73394495412845 256 Q 368.73394495412845 285.7492354740061 355.4250764525994 310.0183486238532 L 324.8929663608563 286.5321100917431 L 324.8929663608563 286.5321100917431 Q 335.0703363914373 263.04587155963304 328.8073394495413 236.42813455657492 Q 321.76146788990826 212.15902140672782 302.9724770642202 197.28440366972478 Q 283.40061162079513 182.40978593272172 259.13149847094803 180.8440366972477 Q 252.085626911315 182.40978593272172 253.651376146789 190.23853211009174 Q 256 197.28440366972478 256 205.8960244648318 Q 256 218.42201834862385 250.51987767584097 227.8165137614679 L 180.0611620795107 173.01529051987768 L 180.0611620795107 173.01529051987768 Z M 297.49235474006116 360.9051987767584 Q 277.9204892966361 368.73394495412845 256 368.73394495412845 Q 208.2446483180428 367.16819571865443 176.14678899082568 335.8532110091743 Q 144.83180428134557 303.75535168195717 143.26605504587155 256 Q 143.26605504587155 248.17125382262998 144.04892966360856 240.34250764525993 L 70.45871559633028 182.40978593272172 L 70.45871559633028 182.40978593272172 Q 43.84097859327217 218.42201834862385 32.88073394495413 246.60550458715596 Q 28.966360856269112 256 32.88073394495413 265.39449541284404 Q 41.49235474006116 287.31498470948014 59.49847094801223 314.7155963302752 Q 77.5045871559633 342.11620795107035 105.68807339449542 367.9510703363914 Q 133.0886850152905 394.56880733944956 170.66666666666666 412.5749235474006 Q 208.2446483180428 430.5810397553517 256 431.3639143730887 Q 312.3669724770642 429.79816513761466 354.64220183486236 406.3119266055046 L 297.49235474006116 360.9051987767584 L 297.49235474006116 360.9051987767584 Z" />
+            </svg>
+          </div>
         </div>
       </div>
 
@@ -158,6 +169,7 @@ export default {
       isProcessing: true,
       isSubscriptionProcessing: true,
       accountShimmerItems: Array(4).fill(null),
+      isAccountIDBlurred: true
     };
   },
   computed: {
@@ -247,8 +259,13 @@ export default {
     }
 
     qr.addData(accId);
+    qr.make();
+    this.$refs.qrcode.innerHTML = qr.createSvgTag(3, 10);
   },
   methods: {
+    toggleAccountIDBlur() {
+      this.isAccountIDBlurred = !this.isAccountIDBlurred;
+    },
     async logOut() {
       // check: is it is necessary to warn user about enabled firewall?
       let isNeedPromptFirewallStatus = false;
@@ -501,5 +518,30 @@ div.detailedParamValue {
   -webkit-user-select: text;
   user-select: text;
   letter-spacing: 0.1px;
+}
+
+// Blur effect for the QR code
+.blurred {
+  filter: blur(5px);
+  /* Adjust the blur radius as needed */
+  transition: filter 0.3s ease;
+  /* Smooth transition for the blur effect */
+}
+
+.overlay-container {
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+}
+
+// QR code overlay
+.overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 5px 5px 2px 5px;
+  border-radius: 4px;
 }
 </style>
