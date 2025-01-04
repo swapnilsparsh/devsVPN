b8949d8bb88dfde53280e66e3ed84b367218f3fa
Vladimir Zbarsky <zbarsky@cornell.edu>
2025-01-03 23:23:19 -0600

    Fail login if we get a reserved IP address (ending in ".0.0" or ".0.1"), show an error.

--- daemon/service/service.go
+++ daemon/service/service.go
@@ -1882,6 +1882,11 @@ func (s *Service) SessionNew(emailOrAcctID string, password string, deviceName s
 	}
 
 	localIP := strings.Split(connectDevSuccessResp.Data[0].Interface.Address, "/")[0]
+	if strings.HasSuffix(localIP, ".0.0") || strings.HasSuffix(localIP, ".0.1") {
+		s.SessionDelete(true, true) // logout
+		return 0, "", preferences.AccountStatus{}, "", log.ErrorFE("Error - got assigned an invalid IP address '%s' when registering a device. Please try "+
+			"logging in again later. Please email support@privateline.io about this problem.", localIP)
+	}
 
 	// get account status info
 	// accountInfo = s.createAccountStatus(sessionNewSuccessResp.ServiceStatus)
@@ -2075,6 +2080,11 @@ func (s *Service) SsoLogin(code string, sessionCode string) (
 	}
 
 	localIP := strings.Split(connectDevSuccessResp.Data[0].Interface.Address, "/")[0]
+	if strings.HasSuffix(localIP, ".0.0") || strings.HasSuffix(localIP, ".0.1") {
+		s.SessionDelete(true, true) // logout
+		return 0, "", nil, log.ErrorFE("Error - got assigned an invalid IP address '%s' when registering a device. Please try "+
+			"logging in again later. Please email support@privateline.io about this problem.", localIP)
+	}
 
 	// get account status info
 	// accountInfo = s.createAccountStatus(sessionNewSuccessResp.ServiceStatus)
