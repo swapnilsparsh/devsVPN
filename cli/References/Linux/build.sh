35f7a0d524f7b629e469040448ceaaf9ed8f2392
Vladimir Zbarsky <zbarsky@cornell.edu>
2025-01-27 13:30:25 -0600

    Adding v4l-utils to install-time package dependencies on Linux

--- cli/References/Linux/build.sh
+++ cli/References/Linux/build.sh
@@ -319,7 +319,7 @@ CreatePackage()
   DEBUG_ARGS=
 #  DEBUG_ARGS="--log debug --debug"
 
-  FPMOUT=$(fpm ${DEBUG_ARGS} -d openvpn -d iptables -d "${RESOLVCONF_DEP}" $EXTRA_ARGS \
+  FPMOUT=$(fpm ${DEBUG_ARGS} -d openvpn -d iptables -d v4l-utils -d "${RESOLVCONF_DEP}" $EXTRA_ARGS \
     --conflicts $CONFLICTS ${PKG_COMPRESSION_ARGS} \
     --rpm-rpmbuild-define "_build_id_links none" \
     --deb-no-default-config-files -s dir -t $PKG_TYPE -n $PKG_NAME -v $VERSION --url https://www.privateline.io --license "GNU GPL3" \
