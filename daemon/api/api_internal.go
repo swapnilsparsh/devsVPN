b17a8b24cce53a648f14a4185a2e9d49730211d1
Vladimir Zbarsky <zbarsky@cornell.edu>
2025-06-30 14:55:45 -0500

    Log REST API requests

--- daemon/api/api_internal.go
+++ daemon/api/api_internal.go
@@ -204,6 +204,8 @@ func (a *API) doRequest(ipTypeRequired types.RequiredIPProtocol, host string, ur
 		}
 	}
 
+	log.Debug(method + " " + host + " " + urlPath)
+
 	if len(host) == 0 || host == a.getApiHost().Hostname {
 		if ipTypeRequired != types.IPvAny {
 			// The specific IP version required to use
