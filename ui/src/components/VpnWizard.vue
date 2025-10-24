9e83dad4fcbffcb79008ededa2866c30c1342944
Vladimir Zbarsky <zbarsky@cornell.edu>
2025-10-24 18:48:46 -0500

    Intro step header text to show in red

--- ui/src/components/VpnWizard.vue
+++ ui/src/components/VpnWizard.vue
@@ -42,7 +42,7 @@
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
-              stroke="currentColor"
+              stroke="#ff0000"
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
